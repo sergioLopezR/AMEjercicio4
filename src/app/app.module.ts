8688d7d5eb452c970a0b2a95bbb160254d7a764e
sergioLopezR <selopezro@ittepic.edu.mx>
2019-10-29 09:54:23 -0700

    Ejercicio 4 - Sergio Lopez Robles y Alondra Gonzalez Cruz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  myForm: FormGroup;
+  
+  constructor(private fb:FormBuilder) {}
+
+  //Se lanza despues de que se cargan todos los componentes de html
+  ngOnInit(){
+    this.myForm = this.fb.group({
+      rfc: ["", Validators.compose([Validators.required, Validators.minLength(13), Validators.pattern('[A-Z,Ñ,&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Z,0-9]?[A-Z,0-9]?[0-9,A-Z]?')])],
+      name: ["", Validators.compose([Validators.required, Validators.minLength(10)])],  //["Valor inicial del input", ]
+      email: ["", Validators.compose([Validators.required, Validators.maxLength(320), Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])],
+      genero: ["", Validators.required],
+      curp: ["", Validators.compose([Validators.required, Validators.minLength(18), Validators.pattern('[A-Z][A,E,I,O,U,X][A-Z]{2}[0-9]{2}[0-1][0-9][0-3][0-9][M,H][A-Z]{2}[B,C,D,F,G,H,J,K,L,M,N,Ñ,P,Q,R,S,T,V,W,X,Y,Z]{3}[0-9,A-Z][0-9]')])]
+    });
+  }
 
 }
